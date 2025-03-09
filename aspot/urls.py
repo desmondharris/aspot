117d3949ce6d9e3b3fb72fa237f0c9d3d32ffe1b
Desmond Harris <dkharr05@louisville.edu>
2025-03-09 12:02:12 -0400

    add login routes

--- aspot/urls.py
+++ aspot/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('spotify/', include('spotifylayer.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
