35eb349077ac28d3a11be390433fa5cc4e8438c3
Desmond Harris <dkharr05@louisville.edu>
2025-03-09 12:04:31 -0400

    add spotify token model

--- spotifylayer/models.py
+++ spotifylayer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SpotifyToken(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=500)
+    refresh_token = models.CharField(max_length=500)
+    token_type = models.CharField(max_length=50)
+    expires_in = models.DateTimeField()
