9b79cf0c501d79052d84b61bfb5001115e17b03c
Desmond Harris <dkharr05@louisville.edu>
2025-03-09 12:02:20 -0400

    add login routes

--- spotifylayer/urls.py
+++ spotifylayer/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("likedsongs/", views.likedsongs, name="likedsongs"),
+    path("login/", views.spotify_login, name="spotify_login"),
+    path("callback/", views.spotify_callback, name="spotify_callback"),
 ]
