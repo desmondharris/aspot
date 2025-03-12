d2dbdbc2713dfd85d2d5af072263adf2bc62a37a
Desmond Harris <dkharr05@louisville.edu>
2025-03-12 00:22:37 -0400

    add spotifyuser table

--- spotifylayer/models.py
+++ spotifylayer/models.py
@@ -3,8 +3,17 @@ from django.contrib.auth.models import User
 
 
 class SpotifyToken(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     access_token = models.CharField(max_length=500)
     refresh_token = models.CharField(max_length=500)
     token_type = models.CharField(max_length=50)
     expires_in = models.DateTimeField()
+
+
+class SpotifyUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    display_name = models.CharField(max_length=50)
+    spotify_id = models.CharField(max_length=75)
+    spotify_uri = models.CharField(max_length=100)
+    profile_image = models.URLField(max_length=200, null=True)
+
