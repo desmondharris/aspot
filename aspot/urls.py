bf566c57b2bab5c265093b84fb0b27f189882442
Desmond Harris <dkharr05@louisville.edu>
2025-03-12 00:22:21 -0400

    direct / to spotifylayer

--- aspot/urls.py
+++ aspot/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('spotify/', include('spotifylayer.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("", include('spotifylayer.urls')),
 ]
--- spotifylayer/urls.py
+++ spotifylayer/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
     path("likedsongs/", views.liked_songs, name="liked_songs"),
     path("login/", views.spotify_login, name="spotify_login"),
     path("callback/", views.spotify_callback, name="spotify_callback"),
